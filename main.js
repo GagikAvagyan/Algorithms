c4bad15a0733e21772d17fd90ea7ed73f011ebd7
Gagik <65705010+GagikAvagyan@users.noreply.github.com>
2021-09-12 15:14:20 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-
-
 let array = [66, 88, 100, 40, 115, 78, 65, 62, 35, 126, 20, 56, 110, 95, 26, 47, 56, 80, 45, 65];
 let arr;
+let arrSel;
 
-function createArray(){
+
+// Bubble Sorting
+function createArrayForBubble(){
    arr = Array.from((array))
 }
-createArray()
+createArrayForBubble()
 
-function printInToHtml(){
+function printInToHtmlBuble(){
    $(".BubbleContainer").empty();
    for( let i = 0; i < arr.length; i++){
       $(".BubbleContainer").append(`
@@ -16,7 +17,7 @@ function printInToHtml(){
       `);
    }
 }
-printInToHtml()
+printInToHtmlBuble()
 
 function print() {
    for (let i = 0; i < arr.length; i++) {
@@ -25,7 +26,7 @@ function print() {
             let Switch = arr[j];
             arr[j] = arr[j + 1];
             arr[j + 1] = Switch;
-            printInToHtml()
+            printInToHtmlBuble()
          }
       }
       break
@@ -36,6 +37,53 @@ setInterval(function (){
 }, 250);
 
 $(".bubbleSorting").click(function (){
-   createArray();
+   createArrayForBubble();
 })
 
+// Bubble Sorting
+
+
+
+// selectionSorting
+
+function createArrayForSelection(){
+   arrSel = Array.from(array)
+}
+createArrayForSelection();
+
+function printInToHtmlSelection(){
+   $(".SelectionContainer").empty();
+   for( let i = 0; i < arrSel.length; i++){
+      $(".SelectionContainer").append(`
+         <div class="item" style="height: ${arrSel[i]}px;"></div>
+      `);
+   }
+}
+printInToHtmlSelection();
+
+function PrintSel(){
+   for (let i = 0; i < arrSel.length; i++) {
+
+      let min = i;
+      for (let j = i + 1; j < arrSel.length; j++) {
+         if (arrSel[j] < arrSel[min]) {
+            min = j;
+            break
+         }
+      }
+
+      if (min != i) {
+         let tmp = arrSel[i];
+         arrSel[i] = arrSel[min];
+         arrSel[min] = tmp;
+         printInToHtmlSelection()
+      }
+   }
+}
+setInterval(function (){
+   PrintSel();
+}, 250);
+
+$(".selectionSorting").click(function (){
+   createArrayForSelection();
+})
